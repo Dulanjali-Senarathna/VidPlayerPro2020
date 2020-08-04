6c733da030174eafa4ce1b2e54861066a5f9cecb
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-08-04 19:51:53 +0530

    Create Video Model

--- app/src/main/java/com/example/vidplayerpro/MainActivity.java
+++ app/src/main/java/com/example/vidplayerpro/MainActivity.java
@@ -6,11 +6,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.database.Cursor;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
+import com.example.vidplayerpro.Models.VideoModel;
+
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.recyclerView) RecyclerView recyclerView;
+
+    ArrayList<VideoModel> arrayListVideos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        arrayListVideos = new ArrayList<>();
+
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));
         recyclerView.setHasFixedSize(true);
         
@@ -26,5 +43,80 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayAllVideos() {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data,thum;
+
+        String absolutePathThumb = null;
+
+        uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+
+        String [] projection = {MediaStore.MediaColumns.DATA, MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
+        MediaStore.Video.Media._ID,MediaStore.Video.Thumbnails.DATA};
+
+        final String orderBy = MediaStore.Video.Media.DEFAULT_SORT_ORDER;
+
+        cursor = MainActivity.this.getContentResolver().query(uri,projection,null,null,orderBy);
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+
+        thum = cursor.getColumnIndexOrThrow(MediaStore.Video.Thumbnails.DATA);
+
+        while (cursor.moveToNext())
+        {
+            absolutePathThumb = cursor.getString(column_index_data);
+            Uri thumbUri = Uri.fromFile(new File(absolutePathThumb));
+            String cursorThumb = cursor.getString(thum);
+
+            String fileName = FilenameUtils.getBaseName(absolutePathThumb);
+            String extension = FilenameUtils.getExtension(absolutePathThumb);
+
+            String duration = getDuration(absolutePathThumb);
+
+            VideoModel videoModel = new VideoModel();
+            videoModel.setDuration(duration);
+            videoModel.setVideo_uri(thumbUri.toString());
+            videoModel.setVideo_path(absolutePathThumb);
+            videoModel.setVideo_name(fileName);
+            if(extension!=null)
+            {
+                videoModel.setVideo_extension(extension);
+            }
+            else {
+                videoModel.setVideo_extension("mp4");
+            }
+
+            if(duration!=null)
+            {
+                videoModel.setDuration(duration);
+            }
+            else {
+                videoModel.setDuration("00:00");
+            }
+
+            arrayListVideos.add(videoModel);
+
+        }
+
+        //set adapter
     }
+
+    private String getDuration(String absolutePathThumb)
+    {
+        try {
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            retriever.setDataSource(this,Uri.fromFile(new File(absolutePathThumb)));
+            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+            long timeInMilliSec = Long.parseLong(time);
+            retriever.release();
+
+            return (new SimpleDateFormat("mm:ss")).format(new Date(timeInMilliSec));
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return  null;
+        }
+        }
 }
---
+++ app/src/main/java/com/example/vidplayerpro/Models/VideoModel.java
@@ -0,0 +1,54 @@
+package com.example.vidplayerpro.Models;
+
+public class VideoModel {
+
+    public String video_thumb,video_uri,video_path,video_name,video_extension,duration;
+
+    public String getVideo_thumb() {
+        return video_thumb;
+    }
+
+    public void setVideo_thumb(String video_thumb) {
+        this.video_thumb = video_thumb;
+    }
+
+    public String getVideo_uri() {
+        return video_uri;
+    }
+
+    public void setVideo_uri(String video_uri) {
+        this.video_uri = video_uri;
+    }
+
+    public String getVideo_path() {
+        return video_path;
+    }
+
+    public void setVideo_path(String video_path) {
+        this.video_path = video_path;
+    }
+
+    public String getVideo_name() {
+        return video_name;
+    }
+
+    public void setVideo_name(String video_name) {
+        this.video_name = video_name;
+    }
+
+    public String getVideo_extension() {
+        return video_extension;
+    }
+
+    public void setVideo_extension(String video_extension) {
+        this.video_extension = video_extension;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
